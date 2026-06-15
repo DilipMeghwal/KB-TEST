842c7888d4d6413a843314c280b334d3946bbfce
Dilip Meghwal <dilipmeghwal@Mac.lan>
2026-06-15 15:30:38 +0530

    config update

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 reporter: [
-    ['line'], // Optional: Keeps standard console output
+    ['html'], // If you want to keep the standard HTML report
     ['allure-playwright', { outputFolder: 'allure-results' }]
   ],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
